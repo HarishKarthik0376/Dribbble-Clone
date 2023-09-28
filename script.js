51e00684a8fc904c9766ede9ff2c89ec1057278f
HarishKarthik0376 <103718125+HarishKarthik0376@users.noreply.github.com>
2023-09-29 01:38:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add("show");
+        }
+        else
+        {
+            entry.target.classList.remove("show");
+        }
+    })
+})
+const elements = document.querySelectorAll(".maintext");
+elements.forEach((el)=> observer.observe(el));
+const hidelements = document.querySelectorAll(".maintext2");
+hidelements.forEach((el)=> observer.observe(el));
+const hidelements1 = document.querySelectorAll(".start");
+hidelements1.forEach((el)=> observer.observe(el));
+
+const observer2 = new IntersectionObserver((entries2) =>{
+entries2.forEach((entry2)=>{
+    console.log(entry2);
+    if(entry2.isIntersecting){
+        entry2.target.classList.add("hey1");
+    }
+    else
+    {
+        entry2.target.classList.remove("hey1");
+    }
+})
+})
+const hidelements2 = document.querySelectorAll(".pink");
+hidelements2.forEach((el)=> observer2.observe(el));
+const hidelements3 = document.querySelectorAll(".slideimages");
+hidelements3.forEach((el)=> observer2.observe(el));
+const hidelements4 = document.querySelectorAll(".justtext");
+hidelements4.forEach((el)=> observer2.observe(el));
